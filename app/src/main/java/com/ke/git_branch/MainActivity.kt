4c43c6e0851f10df6004d33d99dbe878d81325f5
余艳辉 <913918146@qq.com>
2021-12-20 10:04:11 +0800

    Initial commit

---
+++ app/src/main/java/com/ke/git_branch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ke.git_branch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
