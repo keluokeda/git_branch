97a792ba6c211917823f4048a9366d95c31b0421
余艳辉 <913918146@qq.com>
2021-12-20 10:21:50 +0800

    移除所有合并的代码

--- app/src/main/java/com/ke/git_branch/MainActivity.kt
+++ app/src/main/java/com/ke/git_branch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ke.git_branch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,11 +11,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    private fun showDialog(){
 
-        AlertDialog.Builder(this)
-            .setTitle("标题")
-            .setMessage("文字")
-            .show()
-    }
 }
