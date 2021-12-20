072d89a89e104ed6a0c40b8aa38cf9308ff532d9
余艳辉 <913918146@qq.com>
2021-12-20 10:10:33 +0800

    增加按钮点击事件

--- app/src/main/java/com/ke/git_branch/MainActivity.kt
+++ app/src/main/java/com/ke/git_branch/MainActivity.kt
@@ -2,14 +2,20 @@ package com.ke.git_branch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(android.R.id.content)
+            .setOnClickListener {
+                showDialog()
+            }
     }
 
-    private fun showDialog(){
+    private fun showDialog() {
 
     }
+
 }
