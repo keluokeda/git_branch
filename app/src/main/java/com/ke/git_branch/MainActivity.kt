b512385bee3048d9f218e5d88cc224c16f8ab1aa
余艳辉 <913918146@qq.com>
2021-12-20 10:22:17 +0800

    移除无效引用

--- app/src/main/java/com/ke/git_branch/MainActivity.kt
+++ app/src/main/java/com/ke/git_branch/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.ke.git_branch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
